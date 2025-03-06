0d3f40d033b86dff9d918154aede0a7f175c7160
beetwenty <sindrebergee96@gmail.com>
2025-03-06 18:15:18 +0100

    Fixed With own webserver

--- android/app/src/main/java/com/au11no/lifted/MainActivity.java
+++ android/app/src/main/java/com/au11no/lifted/MainActivity.java
@@ -2,5 +2,19 @@
 package com.au11no.lifted;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+    }
+}
