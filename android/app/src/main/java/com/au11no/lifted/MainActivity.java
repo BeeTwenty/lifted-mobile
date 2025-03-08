6efb3a87bde0f88d9118d67e9b8fb2e0cd63ef28
beetwenty <sindrebergee96@gmail.com>
2025-03-08 12:09:15 +0100

    background

--- android/app/src/main/java/com/au11no/lifted/MainActivity.java
+++ android/app/src/main/java/com/au11no/lifted/MainActivity.java
@@ -7,31 +7,32 @@ import android.net.Uri;
 import android.os.PowerManager;
 import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        checkBatteryOptimization();
+        requestBatteryOptimizationExemption();
     }
 
-    private void checkBatteryOptimization() {
+    private void requestBatteryOptimizationExemption() {
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
 
         if (pm != null && !pm.isIgnoringBatteryOptimizations(getPackageName())) {
             Log.d("BatteryOptimization", "Requesting battery optimization exemption");
 
-            Intent intent = new Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+            Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
             intent.setData(Uri.parse("package:" + getPackageName()));
-
             try {
                 startActivity(intent);
+                Toast.makeText(this, "Please disable battery optimization for the app.", Toast.LENGTH_LONG).show();
             } catch (Exception e) {
                 Log.e("BatteryOptimization", "Battery optimization request failed", e);
             }
         } else {
-            Log.d("BatteryOptimization", "Battery optimization already ignored.");
+            Log.d("BatteryOptimization", "Battery optimization already disabled.");
         }
     }
 }
